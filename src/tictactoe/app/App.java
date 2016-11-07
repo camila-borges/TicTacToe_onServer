047ff361b330daff845bf02e33945a560a65d06a
brunogalerani <bruno_galerani@hotmail.com>
2016-11-07 21:13:54 -0200

    App ready

--- src/tictactoe/app/App.java
+++ src/tictactoe/app/App.java
@@ -1,5 +1,17 @@
 package tictactoe.app;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
